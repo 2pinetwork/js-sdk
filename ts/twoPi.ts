c8c6af8177073aef8977fff6edf69d086a91250b
Batudo <batudo@2pi.network>
2021-10-01 19:28:06 -0300

    Polygon ABIs

--- ts/twoPi.ts
+++ ts/twoPi.ts
@@ -3,7 +3,7 @@ import { Provider } from '@ethersproject/abstract-provider'
 import Vault from './vault'
 import getVaults from './data/vaults'
 
-type Chain = 80001
+type Chain = 137 | 80001
 
 export default class TwoPi {
   chainId:  Chain
